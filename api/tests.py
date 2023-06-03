77a1ee438d4f2ee9ac89d7d56f0a951012091274
RichardRomero <ric.romero@duocuc.cl>
2023-06-02 20:21:40 -0400

    Test get producto

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class MyAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        # Realiza cualquier configuración necesaria para tus pruebas
+
+    def test_my_api_view(self):
+        url = '/api/producto/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Realiza más aserciones para verificar el comportamiento esperado
