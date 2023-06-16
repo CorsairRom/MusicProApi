9cc20af2aa6473b8d37f29156fa5b05c4d8f04cd
Rodrigo Seguel <FeaRushb@gmail.com>
2023-06-16 02:44:58 -0400

    Testing
    Se agrego el testing al endpoint principal de integracion con WebPay.

--- MusicPro-webpay/main.js
+++ MusicPro-webpay/main.js
@@ -6,7 +6,7 @@ const { Options, IntegrationApiKeys, Environment, IntegrationCommerceCodes } = r
 const app = express();
 const port = 3000 || process.env.PORT;
 
-app.use(express.json());
+app.use(express.json()); // Middleware to parse JSON request bodies
 
 app.get('/', async (req, res) => {
     try {
@@ -19,38 +19,41 @@ app.get('/', async (req, res) => {
       }
 });
 
-app.post('/comprar', async (req, res) => {
-    try {
-        let compra = req.body;
-        let detalleExists = false;
 
-        const values = await axios.get('http://127.0.0.1:8000/api/pedido');
-        values.data.forEach(element => {
-            if (element.id == compra.id) {
-                compra = element
-                detalleExists = true;
-            }
-        });
 
-        if (!detalleExists) {
-            throw new Error("No existe un pedido con el id indicado.")
+  app.post('/comprar', async (req, res) => {
+      try {
+          let compra = req.body;
+          let detalleExists = false;
+
+          const values = await axios.get('http://127.0.0.1:8000/api/pedido');
+          values.data.forEach(element => {
+              if (element.id == compra.id) {
+                  compra = element
+                  detalleExists = true;                   
+              }
+          });
+
+          
+          
+          if (!detalleExists) {
+              throw new Error("No existe un pedido con el id indicado.")
+          }
+          
+          // Send request to Webpay API
+          const tx = new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration));
+          const response = await tx.create(
+              compra.id.toString(),
+              "S" + Math.floor(Math.random() * 1000),
+              compra.subtotal,
+              "http://localhost:3000/confirmacion"
+            );
+
+          res.json("La compra se esta llevando a cabo en: " + response.url + "?token_ws=" + response.token )
+        } catch (error) {
+          res.status(500).send('Error:' + error.message);
         }
-        
-        const tx = new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration));
-        const response = await tx.create(
-            compra.id.toString(),
-            "S" + Math.floor(Math.random() * 1000),
-            compra.total,
-            "http://localhost:3000/confirmacion"
-          );
-
-        console.log(response.url + "?token_ws=" + response.token )
-        res.json("La compra se esta llevando a cabo en: " + response.url + "?token_ws=" + response.token )
-      } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).send('Error:', error.message);
-      }
-});
+  });
 
 app.get('/confirmacion', async (req, res) => {
   try {
@@ -60,18 +63,21 @@ app.get('/confirmacion', async (req, res) => {
       const response = await tx.commit(token);    
 
       if (response.status == 'AUTHORIZED') {
-        console.log(response)
         res.status(200).send('La compra se ha concretado de forma exitosa :D');
-        // enviar por axios el id del pedido a la api de django
       } else {
-        res.status(400).send('Algo ha salido mal :c');
+        res.status(200).send('La compra no se ha autorizado');
       }
     } catch (error) {
-      console.error('Error:', error.message);
-      res.status(500).send('Error:', error.message);
+      console.error('Error a:', error.message);
+      res.status(500).send('Error a:' + error.message);
     }
 });
+
+
 
 app.listen(port, ()=>{
     console.log('la pagina ha funcionado en: http://localhost:' + port);
 })
+
+// descomentar para testing
+// module.exports = app;
